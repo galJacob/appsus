f307851bea049b3b84330f4ab609402fb390b9ff
galJacob <joojonogal2@gmail.com>
2018-07-05 20:13:08 +0300

    dsadsasasa

--- js/routes.js
+++ js/routes.js
@@ -1,6 +1,7 @@
 import emailApp  from '../pages/email-app.js';
 import miserKeep  from '../pages/keep-app.js';
 import home from '../pages/home.js';
+import emailDetails from '../cmps/mister-Email/email-details.js';
 
 
 
@@ -8,5 +9,6 @@ import home from '../pages/home.js';
 export default [
     { path: '/', component: home },
     { path: '/email-app', component: emailApp },
+    { path: '/email-app/:emailId', component: emailDetails },
     { path: '/keep-app', component: miserKeep },
 ]
