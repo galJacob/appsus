70b74ad96ed8955e8fcf5787333af6c802a3b49f
galJacob <joojonogal2@gmail.com>
2018-07-05 15:20:44 +0300

    changed route

--- cmps/nav-bar-cmp.js
+++ cmps/nav-bar-cmp.js
@@ -3,7 +3,7 @@ export default {
     template:`
         <ul class='nav-bar'>
             <li><a class="active" @click="$router.push('/')">Home</a></li>
-            <li><a @click="$router.push('/mister-Email')">Mister Email</a></li>
+            <li><a @click="$router.push('/email-app')">Mister Email</a></li>
             <li><a @click="$router.push('/mister-keep')">Mister Keep</a></li>
             <li><a href="#about">About</a></li>
         </ul>
